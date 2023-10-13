30f3f9b342dabf7778ec828444c7780d47f9d88d
joacomedel <joaquinmedel1234@gmail.com>
2023-10-12 21:05:20 -0300

    header por js , carrusel andando

--- js/header.js
+++ js/header.js
@@ -1,6 +1,9 @@
 fetch("/json/juegos.json").then(response => response.json()).then(param => cargarHeader(param));
 
 function cargarHeader(arregloJuegos) {
+    var header = document.createElement("header");
+    header.innerHTML = '<nav><div class="espacioLogo"> <a href="index.html" class="anchorLogo" id="seleccionado"><h1>NOMBRE EMPRESA</h1> </a></div> <div class="espacioLinks"> <a href=""> COMPRA MONEDAS </a><div class="divJuegosHeader"><a href="/html/juegos.html">JUEGOS</a><ul class="dropdown"></ul></div><a href="">FAQ</a><a href="">COTIZACION</a></div></nav>';
+    document.querySelector("body").insertBefore(header, document.querySelector(".divPrincipal"));
     const lista = document.querySelector('.divJuegosHeader ul');
     arregloJuegos.forEach(libreria => {
         const li = document.createElement("li");
@@ -12,4 +15,8 @@ function cargarHeader(arregloJuegos) {
         a.href = "Descargas/" + tituloSinEspacios + ".html";
     });
 
+}
+
+function getHtmlHeader() {
+    return
 }
--- js/juegos.js
+++ js/juegos.js
@@ -7,13 +7,13 @@ function postFetch(param) {
     window.addEventListener('resize', emparejarUltimoDiv);
 }
 function cargarDatos(arregloJuegos) {
-    const contenedorJuegos = document.querySelector('.contenedorJuegos');
-    console.log(contenedorJuegos);
-    contenedorJuegos.stylesHeight = "400px"
+    const divJuegos = document.querySelector('.divJuegos');
+    console.log(divJuegos);
+    divJuegos.stylesHeight = "400px"
     for (let index = 0; index < arregloJuegos.length; index++) {
         const juego = arregloJuegos[index];
         const div = document.createElement("div");
-        contenedorJuegos.appendChild(div);
+        divJuegos.appendChild(div);
         const link = document.createElement("a");
         div.appendChild(link);
         const img = document.createElement("img");
@@ -32,10 +32,10 @@ function emparejarUltimoDiv() {
     /*Soluciona el problema cuando el ultimo div queda con una row entera para el solo se hace demasiado grande
     seteando el tamaño como el del primer div
     */
-    const primerDiv = document.querySelector('.contenedorJuegos div:first-child');
+    const primerDiv = document.querySelector('.divJuegos div:first-child');
     const height = primerDiv.clientHeight;
     const width = primerDiv.clientWidth;
-    const ultimoDiv = document.querySelector('.contenedorJuegos  div:last-child');
+    const ultimoDiv = document.querySelector('.divJuegos  div:last-child');
     if (height != ultimoDiv.clientHeight || width != ultimoDiv.clientWidth) {
         //Si resulta q el ultimo elemento tiene un tamaño distinto , le setamos el maximo 
         ultimoDiv.style.maxWidth = width + "px";
